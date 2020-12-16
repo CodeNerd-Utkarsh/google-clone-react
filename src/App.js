24b52944072e942bc3eb7ca7c5dce592f383658d
HBASEHQ <hbase_groups_official@aol.com>
2020-12-17 03:24:03 +0530

    working on home.jsx for first page development

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+      <Router >
+        <Switch>
+        <Route path="/search">
+            <h1>This is search page</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Home.css';
+import {Link} from 'react-router-dom';
+import AppsIcon from '@material-ui/icons/Apps';
+import {Avatar} from '@material-ui/core';  
+
+const Home = () => {
+    return (
+        <div>
+            {/* <h1>This is the Homepage.</h1> */}
+
+            <div className="home__header">
+                {/* home page haeder */}
+                <div className="home__headerLeft">
+                    {/* link */}
+                    <Link to='about' >About</Link>
+                    {/* link */}
+                    <Link to='search' >Store</Link>
+                </div>
+                <div className="home__headerRight">
+                    {/* link */}
+                    <Link to='/gmail'>Gmail</Link>
+                    {/* link */}
+                    <Link to='/images'>Images</Link>
+                    {/* icon */}
+                    <AppsIcon />
+                    {/* avatar */}
+                    <Avatar />
+                </div>
+            </div>
+            <div className="home__body">
+                {/* body of the home page */}
+                <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
